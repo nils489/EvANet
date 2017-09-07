cef1cc8954542a29eea8ee97c30d0d7626da79c6
Nils Kornfeld <LNelson@gmx.de>
2017-09-07 14:08:54 +0200

    added unfinished version of EvANet in plain tf

---
+++ EvANet_tf.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+num_classes = 10
+num_epochs  = 50
+batch_size  = 128
+
+(X, Y), (test_x, test_y) = tf.contrib.keras.datasets.cifar10.load_data()
+X = X.astype('float32')
+test_x = test_x.astype('float32')
+
+Y = tf.contrib.keras.utils.to_categorical(Y, num_classes)
+test_y = tf.contrib.keras.utils.to_categorical(test_y, num_classes)
+
+def conv_norm_block(data, n_filters, filter_size):
+    tmptens = tf.layers.conv2d(data, n_filters, filter_size, padding="same")
+    tmptens = tf.layers.batch_normalization(tmptens, training=is_training)
+    return tf.nn.relu(tmptens)
+
+
+
+def evanet(data, n_filters, filter_size):
+    tmptens = conv_norm_block(data, n_filters, filter_size)
+
+    tmptens = tf.layers.average_pooling2d(tmptens, pool_size(3,3),
+                                          strides=(1,1), padding="same")
+    net_out = tf.layers.dense(tmptens, num_classes)
+    return net_out
+
+def train_evanet(X):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(n_epochs):
+            epoch_loss = 0
+            for iters in range(int(X.shape[0]//batch_size)):
+                epoch_x = X[iters*batch_size:]
+
+
+
+print("X.shape: ", X.shape)
+print("Y.shape: ", Y.shape)
+print("test_x.shape: ", test_x.shape)
+print("test_y.shape: ", test_y.shape)
