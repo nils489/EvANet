b8602b766a5f7ab5f4ed6c90d82a5e4d7d2b0687
LNelson89 <LNelson@gmx.de>
2017-12-07 14:32:43 +0100

    replaced module keras with tf.keras

--- EvANet_keras.py
+++ EvANet_keras.py
@@ -1,100 +1,103 @@
-from keras.datasets import cifar10
-from keras.layers.convolutional import Conv2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.core import Activation, Dense, Flatten
-from keras.layers import Input
-from keras.layers.merge import Add, Concatenate
-from keras.layers.pooling import MaxPooling2D, AveragePooling2D
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Model
-from keras.utils import to_categorical
+#from keras.datasets import cifar10
+#from keras.layers.convolutional import Conv2D
+#from keras.layers.normalization import BatchNormalization
+#from keras.layers.core import Activation, Dense, Flatten
+#from keras.layers import Input
+#from keras.layers.merge import Add, Concatenate
+#from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+#from keras.preprocessing.image import ImageDataGenerator
+#from keras.models import Model
+#from keras.utils import to_categorical
 
+import tensorflow as tf
 
 def conv_norm_block(in_blob, width, filter_size):
-    tmpnet = Conv2D(width, filter_size, padding='same')(in_blob)
-    tmpnet = BatchNormalization()(tmpnet)
-    tmpnet = Activation('relu')(tmpnet)
-    return tmpnet
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size,
+                              padding='same')(in_blob)
+    tmpnet = tf.keras.layers.BatchNormalization()(tmpnet)
+    return tf.keras.layers.Activation('relu')(tmpnet)
 
 def res_block_2015(in_blob, width, filter_size):
     tmpnet = conv_norm_block(in_blob, width, filter_size)
-    tmpnet = Conv2D(width, filter_size, padding='same')(tmpnet)
-    tmpnet = BatchNormalization()(tmpnet)
-    tmpnet = Add()([in_blob, tmpnet])
-    tmpnet = Activation('relu')(tmpnet)
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.BatchNormalization()(tmpnet)
+    tmpnet = tf.keras.layers.Add()([in_blob, tmpnet])
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
     return tmpnet
 
 def res_block_2016(in_blob, width, filter_size):
-    tmpnet = BatchNormalization()(in_blob)
-    tmpnet = Activation('relu')(tmpnet)
-    tmpnet = Conv2D(width, filter_size, padding='same')(tmpnet)
-    tmpnet = BatchNormalization()(tmpnet)
-    tmpnet = Activation('relu')(tmpnet)
-    tmpnet = Conv2D(width, filter_size, padding='same')(tmpnet)
-    tmpnet = Add()([in_blob, tmpnet])
+    tmpnet = tf.keras.layers.BatchNormalization()(in_blob)
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.BatchNormalization()(tmpnet)
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.Add()([in_blob, tmpnet])
     return tmpnet
 
 def no_relu(in_blob, width, filter_size):
     tmpnet = conv_norm_block(in_blob, width, filter_size)
-    tmpnet = Conv2D(width, filter_size, padding='same')(tmpnet)
-    tmpnet = BatchNormalization()(tmpnet)
-    tmpnet = Add()([in_blob, tmpnet])
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.BatchNormalization()(tmpnet)
+    tmpnet = tf.keras.layers.Add()([in_blob, tmpnet])
     return tmpnet
 
 def res_conv_block_2016(in_blob, width, filter_size):
-    tmpnet = BatchNormalization()(in_blob)
-    tmpnet = Activation('relu')(tmpnet)
-    tmpnet = Conv2D(width, filter_size, padding='same')(tmpnet)
-    tmpnet = BatchNormalization()(tmpnet)
-    tmpnet = Activation('relu')(tmpnet)
-    tmpnet = Conv2D(widht, filter_size, padding='same')(tmpnet)
-    shortcut = BatchNormalization()(in_blob)
-    shortcut = Activation('relu')(shortcut)
-    shortcut = Conv2D(width, filter_size, padding='same')(shortcut)
-    tmpnet = Add()([shortcut, tmpnet])
+    tmpnet = tf.keras.layers.BatchNormalization()(in_blob)
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.BatchNormalization()(tmpnet)
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
+    tmpnet = tf.keras.layers.Conv2D(widht, filter_size, padding='same')(tmpnet)
+    shortcut = tf.keras.layers.BatchNormalization()(in_blob)
+    shortcut = tf.keras.layers.Activation('relu')(shortcut)
+    shortcut = tf.keras.layers.Conv2D(width, filter_size, padding='same')(shortcut)
+    tmpnet = tf.keras.layers.Add()([shortcut, tmpnet])
     return tmpnet
 
 def no_act_block_2015(in_blob, width, filter_size):
     tmpnet = conv_norm_block(in_blob, width, filter_size)
-    tmpnet = Conv2D(widht, filter_size, padding='same')(tmpnet)
-    tmpnet = Add()([in_blob, tmpnet])
+    tmpnet = tf.keras.layers.Conv2D(widht, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.Add()([in_blob, tmpnet])
     return tmpnet
 
 def bn_after_add_block(in_blob, width, filter_size):
     tmpnet = conv_norm_block(in_blob, width, filter_size)
-    tmpnet = Conv2D(width, filter_size, padding='same')(tmpnet)
-    tmpnet = Add()([in_blob, tmpnet])
-    tmpnet = BatchNormalization()(tmpnet)
-    tmpnet = Activation('relu')(tmpnet)
+    tmpnet = tf.keras.layers.Conv2D(width, filter_size, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.Add()([in_blob, tmpnet])
+    tmpnet = tf.keras.layers.BatchNormalization()(tmpnet)
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
     return tmpnet
 
 def inception_v1_block(in_blob, inc1_width, inc3_width, inc5_width, pool_width,
                        out_width):
-    tmpnet = BatchNormalization()(in_blob)
-    tmpnet = Activation('relu')(tmpnet)
-    inc1net = Conv2D(inc1_width, 1, padding='same')(tmpnet)
-    inc1net = Activation('relu')(inc1net)
-    inc3net = Conv2D(inc3_width//2, 1, padding='same')(tmpnet)
-    inc3net = Activation('relu')(inc3net)
-    inc3net = Conv2D(inc3_width, 3, padding='same')(inc3net)
-    inc3net = Activation('relu')(inc3net)
-    inc5net = Conv2D(inc5_width//2, 1, padding='same')(tmpnet)
-    inc5net = Activation('relu')(inc5net)
-    inc5net = Conv2D(inc5_width, 5, padding='same')(inc5net)
-    inc5net = Activation('relu')(inc5net)
-    poolnet = MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='same')(tmpnet)
-    poolnet = Conv2D(pool_width, 1, padding='same')(poolnet)
-    poolnet = Activation('relu')(poolnet)
-    tmpnet = Concatenate(axis=3)([tmpnet, inc1net, inc3net, inc5net, poolnet])
-    tmpnet = Conv2D(out_width, 1, padding='same')(tmpnet)
-    tmpnet = Add()([in_blob, tmpnet])
+    tmpnet = tf.keras.layers.BatchNormalization()(in_blob)
+    tmpnet = tf.keras.layers.Activation('relu')(tmpnet)
+    inc1net = tf.keras.layers.Conv2D(inc1_width, 1, padding='same')(tmpnet)
+    inc1net = tf.keras.layers.Activation('relu')(inc1net)
+    inc3net = tf.keras.layers.Conv2D(inc3_width//2, 1, padding='same')(tmpnet)
+    inc3net = tf.keras.layers.Activation('relu')(inc3net)
+    inc3net = tf.keras.layers.Conv2D(inc3_width, 3, padding='same')(inc3net)
+    inc3net = tf.keras.layers.Activation('relu')(inc3net)
+    inc5net = tf.keras.layers.Conv2D(inc5_width//2, 1, padding='same')(tmpnet)
+    inc5net = tf.keras.layers.Activation('relu')(inc5net)
+    inc5net = tf.keras.layers.Conv2D(inc5_width, 5, padding='same')(inc5net)
+    inc5net = tf.keras.layers.Activation('relu')(inc5net)
+    poolnet = tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1),
+                                           padding='same')(tmpnet)
+    poolnet = tf.keras.layers.Conv2D(pool_width, 1, padding='same')(poolnet)
+    poolnet = tf.keras.layers.Activation('relu')(poolnet)
+    tmpnet = tf.keras.layers.Concatenate(axis=3)([tmpnet, inc1net, inc3net,
+                                                  inc5net, poolnet])
+    tmpnet = tf.keras.layers.Conv2D(out_width, 1, padding='same')(tmpnet)
+    tmpnet = tf.keras.layers.Add()([in_blob, tmpnet])
     return tmpnet
 
 num_classes = 10
 num_epochs = 200
 batch_size = 128
 
-(X, Y), (test_x, test_y) = cifar10.load_data()
+(X, Y), (test_x, test_y) = tf.keras.datasets.cifar10.load_data()
 
 #X = X.reshape([-1,32,32,3])
 #test_x = test_x.reshape([-1,32,32,3])
@@ -103,16 +106,16 @@ text_x = test_x.astype('float32')
 #X /= 255
 #test_x /= 255
 
-Y = to_categorical(Y, num_classes)
-test_y = to_categorical(test_y, num_classes)
+Y = tf.keras.utils.to_categorical(Y, num_classes)
+test_y = tf.keras.utils.to_categorical(test_y, num_classes)
 
-datagen = ImageDataGenerator(featurewise_center=True,
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(featurewise_center=True,
                              featurewise_std_normalization = True,
                              width_shift_range=0.25, height_shift_range=0.25,
                              vertical_flip=True)
 
 datagen.fit(X)
-a = Input(shape=(32,32,3))
+a = tf.keras.Input(shape=(32,32,3))
 
 evanet = conv_norm_block(a, 16, 3)
 
@@ -124,7 +127,7 @@ evanet = inception_v1_block(evanet, 8, 16, 32, 8, 16)
 evanet = inception_v1_block(evanet, 8, 16, 32, 8, 16)
 evanet = inception_v1_block(evanet, 8, 16, 32, 8, 16)
 
-evanet = Conv2D(32, 1, strides=2, padding='same')(evanet)
+evanet = tf.keras.layers.Conv2D(32, 1, strides=2, padding='same')(evanet)
 
 evanet = res_block_2015(evanet, 32, 3)
 evanet = inception_v1_block(evanet, 16, 32, 64, 16, 32)
@@ -134,7 +137,7 @@ evanet = inception_v1_block(evanet, 16, 32, 64, 16, 32)
 evanet = inception_v1_block(evanet, 16, 32, 64, 16, 32)
 evanet = inception_v1_block(evanet, 16, 32, 64, 16, 32)
 
-evanet = Conv2D(64, 1, strides=2, padding='same')(evanet)
+evanet = tf.keras.layers.Conv2D(64, 1, strides=2, padding='same')(evanet)
 
 evanet = inception_v1_block(evanet, 32, 64, 128, 32, 64)
 evanet = inception_v1_block(evanet, 32, 64, 128, 32, 64)
@@ -144,15 +147,15 @@ evanet = inception_v1_block(evanet, 32, 64, 128, 32, 64)
 evanet = inception_v1_block(evanet, 32, 64, 128, 32, 64)
 evanet = inception_v1_block(evanet, 32, 64, 128, 32, 64)
 
-evanet = BatchNormalization()(evanet)
-evanet = Activation('relu')(evanet)
-evanet = AveragePooling2D(pool_size=(3, 3), strides=(1,1),
+evanet = tf.keras.layers.BatchNormalization()(evanet)
+evanet = tf.keras.layers.Activation('relu')(evanet)
+evanet = tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(1,1),
                           padding='same')(evanet)
 
-evanet = Flatten()(evanet)
-evanet_out = Dense(num_classes, activation='softmax')(evanet)
+evanet = tf.keras.layers.Flatten()(evanet)
+evanet_out = tf.keras.layers.Dense(num_classes, activation='softmax')(evanet)
 
-model = Model(inputs=a, outputs=evanet_out)
+model = tf.keras.models.Model(inputs=a, outputs=evanet_out)
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['acc'])
 print("X.shape: ", X.shape)
 print("Y.shape: ", Y.shape)
